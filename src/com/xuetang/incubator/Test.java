5ee0df01495b2cf3954b0807a6ce4ec509731fd6
incubator231 <553077454@qq.com>
2019-07-16 03:46:36 +0800

    第一次提交

---
+++ src/com/xuetang/incubator/Test.java
@@ -0,0 +1,12 @@
+package com.xuetang.incubator;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("第一次用Git");
+	}
+
+}
